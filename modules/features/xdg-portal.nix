53e38bffbdb3f8b9942b86ec7cc4c37597b62674
ManeeshWije <m.mwije1@proton.me>
2026-07-31 15:20:46 -0400

    wip

--- modules/features/xdg-portal.nix
+++ modules/features/xdg-portal.nix
@@ -1,14 +1,23 @@
 {self, ...}: {
   flake.nixosModules.xdg-portal = {pkgs, ...}: {
-    xdg.portal = {
-      enable = true;
-      extraPortals = [pkgs.xdg-desktop-portal-wlr];
-      config.common.default = ["wlr"];
+xdg.portal = {
+  enable = true;
 
-      wlr.settings.screencast = {
-        chooser_type = "dmenu";
-        chooser_cmd = "${self.packages.${pkgs.stdenv.hostPlatform.system}.noctalia}/bin/noctalia dmenu";
-      };
-    };
+  extraPortals = with pkgs; [
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-wlr
+  ];
+
+  config.common.default = [
+    "gtk"
+    "wlr"
+  ];
+
+  wlr.settings.screencast = {
+    chooser_type = "dmenu";
+    chooser_cmd = "${self.packages.${pkgs.stdenv.hostPlatform.system}.noctalia}/bin/noctalia dmenu";
+  };
+};
   };
 }
+
