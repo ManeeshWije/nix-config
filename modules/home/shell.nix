7e4d65cdc94bb31b7b1d7c2792dbb1260f667b1a
ManeeshWije <m.mwije1@proton.me>
2026-08-06 14:42:45 -0400

    add configs

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -5,7 +5,7 @@ _: {
     inputs,
     ...
   }: {
-    # Have home manager manage itself
+    # Have Home Manager manage itself
     programs.home-manager.enable = true;
 
     home.packages = with pkgs; [
@@ -40,6 +40,19 @@ _: {
       alejandra
     ];
 
+    # ~/.tmux.conf -> dfRoot/tmux/.tmux.conf
+    home.file.".tmux.conf".source =
+      dfRoot + /tmux/.tmux.conf;
+
+    # ~/.config/tms/config.toml -> dfRoot/tmux/tms/config.toml
+    xdg.configFile."tms/config.toml".source =
+      dfRoot + /tmux/tms/config.toml;
+
+    # Explicitly tell tms which config to use.
+    home.sessionVariables = {
+      TMS_CONFIG_FILE = "$HOME/.config/tms/config.toml";
+    };
+
     programs.direnv = {
       enable = true;
       nix-direnv.enable = true;
@@ -48,11 +61,14 @@ _: {
     programs.zsh = {
       enable = true;
       syntaxHighlighting.enable = true;
+
       initContent = ''
         ${builtins.readFile (dfRoot + /.zshrc)}
-          if command -v gh >/dev/null 2>&1 && gh auth status >/dev/null 2>&1; then
-            export GITHUB_PACKAGE_TOKEN="$(gh auth token)"
-          fi
+
+        if command -v gh >/dev/null 2>&1 \
+          && gh auth status >/dev/null 2>&1; then
+          export GITHUB_PACKAGE_TOKEN="$(gh auth token)"
+        fi
       '';
     };
   };
