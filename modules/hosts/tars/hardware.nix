e9b0b22b720a8b4a840bb44d8d4aa60e9b88731b
ManeeshWije <m.mwije1@proton.me>
2026-08-16 23:10:12 -0400

    remove

--- modules/hosts/tars/hardware.nix
+++ modules/hosts/tars/hardware.nix
@@ -5,7 +5,6 @@
     ...
   }: {
     imports = [
-      inputs.nixos-hardware.nixosModules.raspberry-pi-5
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
@@ -14,8 +13,6 @@
       "usbhid"
     ];
 
-    boot.initrd.systemd.tpm2.enable = false;
-
     boot.initrd.kernelModules = [];
     boot.kernelModules = [];
     boot.extraModulePackages = [];
@@ -23,17 +20,6 @@
     fileSystems."/" = {
       device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
       fsType = "ext4";
-    };
-
-fileSystems."/boot" = {
-  device = "/dev/disk/by-uuid/2178-694E";
-  fsType = "vfat";
-};
-
-    fileSystems."/nix" = {
-      device = "/dev/disk/by-uuid/554ec9c7-d831-4a7b-b454-99b469be9f50";
-      fsType = "ext4";
-      neededForBoot = true;
     };
 
     swapDevices = [];
