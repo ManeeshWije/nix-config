ece7cdf944bc875878e00704e1a9734521472874
ManeeshWije <m.mwije1@proton.me>
2026-08-17 21:23:28 -0400

    add sops

--- modules/hosts/tars/configuration.nix
+++ modules/hosts/tars/configuration.nix
@@ -6,6 +6,7 @@
       docker
       pihole
       traefik
+      sops
     ];
 
     # Bootloader.
---
+++ modules/services/sops.nix
@@ -0,0 +1,11 @@
+{inputs, ...}: {
+  flake.nixosModules.sops = {
+    imports = [
+      inputs.sops-nix.nixosModules.sops
+    ];
+
+    sops.age.sshKeyPaths = [
+      "/etc/ssh/ssh_host_ed25519_key"
+    ];
+  };
+}
