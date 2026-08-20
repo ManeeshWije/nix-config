99241d198fd13c222925b55197f20cbda5d081dd
ManeeshWije <m.mwije1@proton.me>
2026-08-19 23:30:36 -0400

    .

--- modules/hosts/gargantua/hardware.nix
+++ modules/hosts/gargantua/hardware.nix
@@ -1,5 +1,6 @@
-{...}: {
+{pkgs, ...}: {
   flake.nixosModules.gargantua = {
+    pkgs,
     lib,
     modulesPath,
     ...
@@ -15,6 +16,20 @@
 
     boot.loader.grub.enable = false;
     boot.loader.generic-extlinux-compatible.enable = true;
+
+    hardware.deviceTree = {
+      enable = true;
+
+      # Only keep the Pi 5 DTB.
+      filter = "bcm2712-rpi-5-b.dtb";
+
+      overlays = [
+        {
+          name = "pcie-32bit-dma-pi5";
+          dtboFile = "${pkgs.raspberrypifw}/share/raspberrypi/boot/overlays/pcie-32bit-dma-pi5.dtbo";
+        }
+      ];
+    };
 
     boot.initrd.kernelModules = [];
     boot.kernelModules = [];
