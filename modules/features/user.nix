63aabc3f5462f8a739ed9a5cd7b5836431a1f854
ManeeshWije <m.mwije1@proton.me>
2026-08-20 22:27:07 -0400

    .

--- modules/features/user.nix
+++ modules/features/user.nix
@@ -1,9 +1,5 @@
 _: {
-  flake.nixosModules.user = {
-    pkgs,
-    config,
-    ...
-  }: {
+  flake.nixosModules.user = {pkgs, ...}: {
     users.users.maneesh = {
       shell = pkgs.zsh;
       isNormalUser = true;
@@ -11,9 +7,9 @@ _: {
       extraGroups = ["networkmanager" "wheel" "docker"];
     };
 
-	users.users.maneesh.openssh.authorizedKeys.keys = [
-	  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJb33lZq6xLBJM3NXyH2WddcipCG1TVAY8YvgPfXMkdH m.mwije1@proton.me"
-	];
+    users.users.maneesh.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJb33lZq6xLBJM3NXyH2WddcipCG1TVAY8YvgPfXMkdH m.mwije1@proton.me"
+    ];
 
     programs.zsh.enable = true;
   };
