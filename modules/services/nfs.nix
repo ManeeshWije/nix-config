bc738cdbf719e4c1ca4a82aec89987bba5d037f7
ManeeshWije <m.mwije1@proton.me>
2026-09-02 18:30:50 -0400

    use uid/gid nfs

--- modules/services/nfs.nix
+++ modules/services/nfs.nix
@@ -1,10 +1,30 @@
 {...}: {
   flake.nixosModules.nfs = {...}: {
+    #
+    # Storage identity
+    #
+    # Every NFS request from Tars is mapped to this user.
+    # qBittorrent/Sonarr/Jellyfin users on Tars therefore do not need
+    # matching UIDs or shared groups.
+    #
+    users.groups.storage = {
+      gid = 2000;
+    };
+
+    users.users.storage = {
+      isSystemUser = true;
+      uid = 2000;
+      group = "storage";
+    };
+
+    #
+    # NFS
+    #
     services.nfs.server = {
-     enable = true;
+      enable = true;
 
       exports = ''
-        /storage 192.168.88.251(rw,sync,no_subtree_check)
+        /storage 192.168.88.251(rw,sync,no_subtree_check,all_squash,anonuid=2000,anongid=2000)
       '';
     };
 
